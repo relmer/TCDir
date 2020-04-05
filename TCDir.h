f0b052d32937092326d5c19c03532bdf61e67128
Rob Elmer <relmer@microsoft.com>
2020-04-05 15:10:48 -0700

    Some TCHAR->Unciode clean up and implement sorting
    Related Work Items: #1

---
+++ TCDir.h
@@ -0,0 +1,21 @@
+#pragma once
+
+
+
+
+
+HANDLE hStdOut;
+
+
+
+
+
+int  _tmain        (int argc, _TCHAR* argv[]);
+int  ConsolePrintf (LPCTSTR pszFormat, ...);
+void Usage         (void);
+
+
+
+
+
+
